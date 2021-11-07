5953bf387e43e64437b5f9c1ef2e27524488247a
Siundae <62743422+Siundae@users.noreply.github.com>
2021-11-07 18:48:38 +1000

    Integrate API for current conditions & location

--- src/script.js
+++ src/script.js
@@ -1,11 +1,27 @@
 function showTemp(response) {
   console.log(response);
-  console.log(response.data.sys.country);
-  console.log(Math.round(response.data.main.temp));
+  console.log(response.data.main.temp_max);
+  let currentTemp = (document.querySelector("#current-temp").innerHTML =
+    Math.round(response.data.main.temp));
+  let currentCity = (document.querySelector("#current-city").innerHTML =
+    response.data.name);
+  let currentCountry = (document.querySelector("#current-country").innerHTML =
+    response.data.sys.country);
+  let currentMin = (document.querySelector("#current-min").innerHTML =
+    Math.round(response.data.main.temp_min));
+  let currentMax = (document.querySelector("#current-max").innerHTML =
+    Math.round(response.data.main.temp_max));
+  let currentConditions = (document.querySelector(
+    "#current-conditions"
+  ).innerHTML = response.data.weather[0].description);
+  let currentHumidity = (document.querySelector("#current-humidity").innerHTML =
+    response.data.main.humidity);
+  let currentWind = (document.querySelector("#current-wind").innerHTML =
+    Math.round(response.data.wind.speed));
 }
 
 let apiKey = "26862e90492f1d5a7f51cde5dcc0d83b";
 let units = "metric";
-let city = "London";
+let city = "Auckland";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
 axios.get(apiUrl).then(showTemp);
