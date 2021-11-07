fb11cbac1b0044591889920ad85a9b2a904d6263
Siundae <62743422+Siundae@users.noreply.github.com>
2021-11-07 19:06:47 +1000

    Added function to format and display day and time

--- src/script.js
+++ src/script.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} | ${hours}:${minutes}`;
+}
+
 function showTemp(response) {
   console.log(response);
   console.log(response.data.main.temp_max);
@@ -18,6 +42,8 @@ function showTemp(response) {
     response.data.main.humidity);
   let currentWind = (document.querySelector("#current-wind").innerHTML =
     Math.round(response.data.wind.speed));
+  let currentDate = (document.querySelector("#current-date").innerHTML =
+    formatDate(response.data.dt * 1000));
 }
 
 let apiKey = "26862e90492f1d5a7f51cde5dcc0d83b";
